d552f16dbda33b7728d1b8b42e34598f23239372
Parris Khachi <parris@storyforj.com>
2020-11-22 17:24:32 -0800

    Include custom components at render time

--- tanuki_ui/src/Renderable.tsx
+++ tanuki_ui/src/Renderable.tsx
@@ -2,24 +2,41 @@ import * as React from "react";
 
 export type RenderableNode = {
   id: string,
-  type: React.ElementType,
+  type: React.ElementType | 'component',
+  componentType?: string,
   props: Object | null,
   children: Array<RenderableNode | String | number> | null,
+  _rootProps?: RenderableRootProps
 }
 
 export type RenderableRootProps = {
   content: {
     version: String,
     children: Array<RenderableNode>,
+    components: Record<string, React.ElementType>,
   },
 };
 
 const RenderNode = (props: RenderableNode) => {
-  const {type, children, ...otherProps} = props;
-  const ComponentType = type;
+  const {type, id, componentType, children, _rootProps, ...otherProps} = props;
+  const isCustom = (type === 'component');
+  const customComponent = props._rootProps?.content.components[componentType ?? ''];
+  // we need to explicitly spell out type === 'component' otherwise we get a ts error down below
+  const ComponentType = (type === 'component') ? (customComponent ?? React.Fragment) : type;
+
+  if (isCustom && !customComponent) {
+    console.warn(`Non-existant component type referenced ${componentType} for node ${id}, using a Fragment instead.`);
+  }
+
+  const nodeProps = {
+    ...otherProps.props,
+    ...(
+      isCustom ? { _rootProps } : {}
+    )
+  }
 
   return (
-    <ComponentType {...otherProps}>
+    <ComponentType {...nodeProps}>
       { children?.map((child) : React.ReactNode => {
         const isImmediatelyRenderable = typeof child === 'number' || typeof child === 'string';
         if (isImmediatelyRenderable) {
@@ -37,7 +54,7 @@ const RenderNode = (props: RenderableNode) => {
 const Renderable = (props: RenderableRootProps) => {
   return (
     <>
-      { props.content.children.map((child) => <RenderNode key={(child as RenderableNode).id} {...child} />) }
+      { props.content.children.map((child) => <RenderNode key={(child as RenderableNode).id} {...child} _rootProps={props} />) }
     </>
   );
 };
--- tanuki_ui/test/Renderable.spec.tsx
+++ tanuki_ui/test/Renderable.spec.tsx
@@ -8,11 +8,34 @@ describe('Renderable', () => {
   it('runs a program tree', function() {
     const file = {
       version: '1.0.0',
+      components: {},
       children: [
         { id: 'a', type: 'div', children: ['testing'], props: { 'className': 'test-name'} } as RenderableNode,
       ],
     };
     const node = mount(<Renderable content={file} />);
     expect(node.text()).to.eq('testing');
+    expect(node.find('.test-name').length).to.eq(1);
+  });
+
+  it('can render a custom component', function() {
+    const file = {
+      version: '1.0.0',
+      components: {
+        'button': ({ className, children }: { className: string, children: React.ElementType }) => (<button className={className}>{children}</button>),
+      },
+      children: [
+        { id: 'a', type: 'div', children: ['testing1'], props: { 'className': 'test-name'} } as RenderableNode,
+        { id: 'b', type: 'component', componentType: 'button', children: ['testing2'], props: { 'className': 'meow'} } as RenderableNode,
+      ],
+    };
+    const node = mount(<Renderable content={file} />);
+    expect(node.text()).to.eq('testing1testing2');
+
+    // NOTE: using find('button') seems a bit unreliable here. The final render does indeed give the correct result.
+    // I believe since we're wrapping our components with our own RenderableNodes we get an incorrect result from enzyme.
+    // IMO seems good to test the final html result at least once anyways *shrug*.
+    expect((node.html().match(/\<button/g) || []).length).to.eq(1);
+    expect((node.html().match(/class\=\"meow\"/g) || []).length).to.eq(1);
   });
 });
