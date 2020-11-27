db51d436e45f3e384f4e86d604449a7358d0bc1b
Parris Khachi <parris@storyforj.com>
2020-11-27 14:29:22 -0800

    Add templated components into the mix

--- tanuki_ui/src/Renderable.tsx
+++ tanuki_ui/src/Renderable.tsx
@@ -1,61 +1,153 @@
 import * as React from "react";
+import * as templateStrings from 'es6-template-string';
 
-export type RenderableNode = {
-  id: string,
-  type: React.ElementType | 'component',
-  componentType?: string,
-  props: Object | null,
-  children: Array<RenderableNode | String | number> | null,
-  _rootProps?: RenderableRootProps
-}
-
-export type RenderableRootProps = {
-  content: {
-    version: String,
-    children: Array<RenderableNode>,
-    components: Record<string, React.ElementType>,
-  },
+export enum Versions {
+  v1 = 'v1.0'
 };
 
-const RenderNode = (props: RenderableNode) => {
-  const {type, id, componentType, children, _rootProps, ...otherProps} = props;
-  const isCustom = (type === 'component');
-  const customComponent = props._rootProps?.content.components[componentType ?? ''];
-  // we need to explicitly spell out type === 'component' otherwise we get a ts error down below
-  const ComponentType = (type === 'component') ? (customComponent ?? React.Fragment) : type;
+export enum ComponentType {
+  module = 'module',
+  template = 'templates',
+};
 
-  if (isCustom && !customComponent) {
-    console.warn(`Non-existant component type referenced ${componentType} for node ${id}, using a Fragment instead.`);
+export type TemplateComponent = {
+  id: string,
+  type: React.ElementType | ComponentType,
+  componentType?: string,
+  options?: object,
+  props?: object | null,
+  nodes?: Array<TemplateComponent | String | number> | null,
+};
+
+export interface RenderableNode extends TemplateComponent {
+  _templatedParentId?: 'string';
+}
+
+export type ComponentDefinition = {
+  availableOptions?: Record<string, any>,
+  template?: TemplateComponent,
+  render?: React.ElementType,
+};
+
+export type Document = {
+  version: Versions,
+  debug?: boolean,
+  styles?: Record<string, React.CSSProperties>,
+  templates?: Record<string, RenderableNode>,
+  components?: Record<string, ComponentDefinition>,
+  body: Array<RenderableNode>,
+};
+
+const fillTemplateString = (templateString: string, inputVars: { options?: object }) => {
+  return templateStrings(templateString, inputVars);
+}
+
+const deepProcessStrings = (obj: Record<string, any>, data: { options?: object }): object => {
+  const newRootObj: Record<string, any> = {...obj};
+  let processed: Record<string, any> = [];
+  let todo: Record<string, any> = [newRootObj];
+
+  while (todo.length) {
+    const currentObj = todo.pop();
+    if (typeof currentObj === 'undefined' || processed.includes(currentObj)) { break; }
+    processed.push(currentObj);
+
+    const currentKeys = Object.keys(currentObj);
+    currentKeys.forEach((key) => {
+      if (typeof currentObj[key] === 'object') {
+        // spread operator ensures all new references here
+        currentObj[key] = { ...currentObj[key] };
+        todo.push(currentObj[key]);
+      } else if (typeof currentObj[key] === 'string') {
+        currentObj[key] = fillTemplateString(currentObj[key], data);
+      }
+    });
   }
 
-  const nodeProps = {
+  return newRootObj;
+};
+
+const TanukiDocumentContext = React.createContext<Document>({ version: Versions.v1, body: [] });
+const useTanukiDocumentContext = () => React.useContext(TanukiDocumentContext);
+
+const RenderNode = (props: RenderableNode) => {
+  let {type, id, componentType, nodes, _templatedParentId, ...otherProps} = props;
+  const rootProps = useTanukiDocumentContext();
+
+  // strange typescript issue here with optional chaining before brackets
+  const customComponent = ((rootProps?.components) ?? {})[(componentType ?? '')];
+  let Component;
+  let templateProps = {};
+
+  if (type === ComponentType.module) {
+    const renderer = customComponent?.render ?? null;
+    if (!renderer) {
+      console.warn(`Non-existant component type referenced ${componentType} for node ${id}, using a Fragment instead.`);
+    }
+    Component = renderer ?? React.Fragment;
+  } else if (type === ComponentType.template) {
+    const template = customComponent?.template ?? null;
+    if (!template) {
+      console.warn(`Non-existant component type referenced ${componentType} for node ${id}, using a Fragment instead.`);
+      Component = React.Fragment;
+    } else {
+      // Template component types must be standard React.ElementTypes. Tanuki Component types are not allowed at the root.
+      Component = template.type as React.ElementType;
+      templateProps = { ...template.props } as TemplateComponent;
+      nodes = template.nodes;
+    }
+  } else {
+    Component = type;
+  }
+
+  let nodeProps = deepProcessStrings({
+    ...(rootProps?.debug ? { 'data-tanuki-id': id } : {}),
+    ...templateProps,
     ...otherProps.props,
     ...(
-      isCustom ? { _rootProps } : {}
+      type === ComponentType.module ? {
+        options: otherProps.options,
+      } : {}
     )
-  }
+  }, { options: otherProps.options });
 
   return (
-    <ComponentType {...nodeProps}>
-      { children?.map((child) : React.ReactNode => {
+    <Component {...nodeProps}>
+      { nodes?.map((child) : React.ReactNode => {
         const isImmediatelyRenderable = typeof child === 'number' || typeof child === 'string';
         if (isImmediatelyRenderable) {
+          if (typeof child === 'string') {
+            return fillTemplateString(child, { options: otherProps.options });
+          }
           return child;
         } else if (['string', 'function'].includes(typeof (child as RenderableNode).type)) {
-          return <RenderNode key={(child as RenderableNode).id} {...(child as RenderableNode)} />;
+          const childProps = (child as RenderableNode);
+
+          const _parentId = type === ComponentType.template ? props.id : _templatedParentId;
+          const hasTemplateParentId = Boolean(_parentId);
+          const id = `${hasTemplateParentId ? `${_parentId}-` : '' }${childProps.id}`;
+          const optionalChildProps = {
+            options: {
+              ...(props.options || {}),
+              ...(childProps.options || {}),
+            },
+            _templateParentId: _parentId,
+          };
+
+          return <RenderNode key={id} {...childProps} id={id} {...optionalChildProps} />;
         }
 
         return null;
-      }) }
-    </ComponentType>
+      }) as React.ReactNode[] }
+    </Component>
   );
 }
 
-const Renderable = (props: RenderableRootProps) => {
+const Renderable = (props: Document) => {
   return (
-    <>
-      { props.content.children.map((child) => <RenderNode key={(child as RenderableNode).id} {...child} _rootProps={props} />) }
-    </>
+    <TanukiDocumentContext.Provider value={props}>
+      { props.body.map((child) => <RenderNode key={(child as RenderableNode).id} {...child} />) }
+    </TanukiDocumentContext.Provider>
   );
 };
 
--- tanuki_ui/test/Renderable.spec.tsx
+++ tanuki_ui/test/Renderable.spec.tsx
@@ -2,34 +2,37 @@ import * as React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
 
-import Renderable, { RenderableNode } from '../src/Renderable';
+import Renderable, { ComponentType, RenderableNode, Versions, TemplateComponent } from '../src/Renderable';
 
 describe('Renderable', () => {
   it('runs a program tree', function() {
     const file = {
-      version: '1.0.0',
+      version: Versions.v1,
       components: {},
-      children: [
-        { id: 'a', type: 'div', children: ['testing'], props: { 'className': 'test-name'} } as RenderableNode,
+      body: [
+        { id: 'a', type: 'div', nodes: ['testing'], props: { 'className': 'test-name'} } as RenderableNode,
       ],
     };
-    const node = mount(<Renderable content={file} />);
+    const node = mount(<Renderable {...file} />);
     expect(node.text()).to.eq('testing');
     expect(node.find('.test-name').length).to.eq(1);
   });
 
-  it('can render a custom component', function() {
+  it('can render a module based component', function() {
     const file = {
-      version: '1.0.0',
+      version: Versions.v1,
       components: {
-        'button': ({ className, children }: { className: string, children: React.ElementType }) => (<button className={className}>{children}</button>),
+        'button': {
+          options: {},
+          render: ({ className, children }: { className: string, children: React.ReactNode }) => (<button className={className}>{children}</button>),
+        }
       },
-      children: [
-        { id: 'a', type: 'div', children: ['testing1'], props: { 'className': 'test-name'} } as RenderableNode,
-        { id: 'b', type: 'component', componentType: 'button', children: ['testing2'], props: { 'className': 'meow'} } as RenderableNode,
+      body: [
+        { id: 'a', type: 'div', nodes: ['testing1'], props: { 'className': 'test-name'} } as RenderableNode,
+        { id: 'b', type: ComponentType.module, componentType: 'button', nodes: ['testing2'], props: { 'className': 'meow'} } as RenderableNode,
       ],
     };
-    const node = mount(<Renderable content={file} />);
+    const node = mount(<Renderable {...file} />);
     expect(node.text()).to.eq('testing1testing2');
 
     // NOTE: using find('button') seems a bit unreliable here. The final render does indeed give the correct result.
@@ -37,5 +40,44 @@ describe('Renderable', () => {
     // IMO seems good to test the final html result at least once anyways *shrug*.
     expect((node.html().match(/\<button/g) || []).length).to.eq(1);
     expect((node.html().match(/class\=\"meow\"/g) || []).length).to.eq(1);
+  });
+
+  it('can render a template component', function() {
+    const file = {
+      version: Versions.v1,
+      components: {
+        'button': {
+          options: {
+            href: 'string',
+            text: 'string',
+          },
+          template: {
+            id: 'abc',
+            type: 'a',
+            props: {
+              href: "${options.href}",
+              style: { 'display': 'block' },
+            },
+            nodes: [
+              {
+                id: 'qed',
+                type: 'span',
+                nodes: ["${options.text}"]
+              }
+            ],
+          } as TemplateComponent,
+        },
+      },
+      body: [
+        { id: 'a', type: 'div', nodes: ['testing1'], props: { 'className': 'test-name'} } as RenderableNode,
+        { id: 'b', type: ComponentType.template, componentType: 'button', options: { href: 'https://tanuki.fun', text: 'Pizza'} } as RenderableNode,
+      ],
+    };
+    const node = mount(<Renderable debug={true} {...file} />);
+    expect(node.text()).to.eq('testing1Pizza');
+
+    expect(node.find('a').first().props().href).to.eq('https://tanuki.fun');
+    expect((node.html().match(/data\-tanuki\-id\=\"b\"/g) || []).length).to.eq(1);
+    expect((node.html().match(/data\-tanuki\-id\=\"b-qed\"/g) || []).length).to.eq(1);
   });
 });
